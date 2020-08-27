ddf44e656d32de9a9d6b98ac9ce6bba722b2b5b8
Erez Lev <erezlev1994@gmail.com>
2020-08-27 12:18:24 +0300

    Updated file
    Add some logic and validations check.

--- User.cs
+++ User.cs
@@ -1,13 +1,29 @@
-﻿using System;
+using System;
+using System.Runtime.CompilerServices;
+using CheckersPiece;
 
 namespace Player
 {
-    class User
+    public struct User
     {
+        // Constants:
+        private const short k_NumberOfCheckerPieces = 12;
+
         // Data members:
-        private string m_Name;
+        private readonly string m_Name;
         private ushort m_Score;
-        // typeOf tools = X\O (eTypeOfTools.FirstPlayer)
+        private static CheckerPiece[] m_CheckersPiece;
+        private readonly char m_CheckerPieceKind;
+
+
+        // Constructors:
+        public User(string i_Name, char i_CheckerKind)
+        {
+            m_Name = i_Name;
+            m_Score = 0;
+            m_CheckersPiece = new CheckerPiece[k_NumberOfCheckerPieces];
+            m_CheckerPieceKind = i_CheckerKind;
+        }
 
         // Properties:
         public string Name
@@ -15,10 +31,6 @@ namespace Player
             get
             {
                 return m_Name;
-            }
-            set
-            {
-                m_Name = value;
             }
         }
 
@@ -34,18 +46,59 @@ namespace Player
             }
         }
 
-        // Constructors:
-        public User(string i_Name)
+        public CheckerPiece[] Pieces
         {
-            m_Name = i_Name;
-            m_Score = 0;
+            get
+            {
+                return m_CheckersPiece;
+            }
         }
 
-        // Methods:
+        public char CheckerKind
+        {
+            get
+            {
+                return m_CheckerPieceKind;
+            }
+        }
 
-        public static void Play()
+
+        // Methods:
+        public static void Play(string i_CheckerPosition, string i_CheckerNextPosition)
         {
             // This method called to play the user's move.
+            CheckerPiece? currentCheckerPiece = findCheckerPiece(i_CheckerPosition);
+
+            if (currentCheckerPiece != null)
+            {
+                if (isRegularChecker(currentCheckerPiece.Value))
+                {
+                    MoveRegularTool(i_CheckerPosition, i_CheckerNextPosition);
+                }
+                else
+                {
+                    MoveKingTool(i_CheckerPosition, i_CheckerNextPosition);
+                }
+            }
+        }
+
+        private static CheckerPiece? findCheckerPiece(string i_CurrentPosition)
+        {
+            CheckerPiece? currentCheckerPiece = null;
+
+            foreach (var piece in m_CheckersPiece)
+            {
+                // if piece.position == i_CurrentPosition
+                // currentCheckerPiece = piece;
+            }
+
+            return currentCheckerPiece;
+        }
+
+        private static bool isRegularChecker(CheckerPiece i_CheckerPiece)
+        {
+            return i_CheckerPiece.PieceKind == CheckerPiece.ePieceKind.FirstPlayer ||
+                   i_CheckerPiece.PieceKind == CheckerPiece.ePieceKind.SecondPlayer;
         }
 
         public static void Quit(string i_QuitInput)
@@ -53,7 +106,7 @@ namespace Player
             // Need to quit the game if the user press q(?).
         }
 
-        public static void MoveRegularTool(string i_Position)
+        public static void MoveRegularTool(string i_PositionFrom, string i_PositionTo)
         {
             // Check valid move - include: inborder, valid input, is empty cell.
             // Update board - new tool position.
@@ -67,7 +120,7 @@ namespace Player
             // Check valid indexes: A-H, a-h.
         }
 
-        public static void MoveKingTool(string i_Position)
+        public static void MoveKingTool(string i_PositionFrom, string i_PositionTo)
         {
             // Check valid move - include: inborder, valid input, is empty cell (for king tool).
             // Update board - new tool position
@@ -85,13 +138,6 @@ namespace Player
         {
             // Check if the given position is in the borders.
             // Means the index is less(!) than border's size.
-
-            return false;   // Temporary return value.
-        }
-
-        private static bool isValidInput(string i_PositionTo)
-        {
-            // Check if the givn input is A-H, a-h (for example).
 
             return false;   // Temporary return value.
         }
