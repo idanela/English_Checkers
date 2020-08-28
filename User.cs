a4503626972dcd665abe726e7b1b701f7cc41d0c
Erez Lev <erezlev1994@gmail.com>
2020-08-28 17:12:29 +0300

    Update User
    Not so many changes.
    Needs to implement findCheckerPiece method.

--- User.cs
+++ User.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Runtime.CompilerServices;
-using CheckersPiece;
+using CheckerPiece;
 using CheckersBoard;
 
 namespace Player
@@ -15,16 +15,25 @@ namespace Player
         // Data members:
         private readonly string m_Name;
         private ushort m_Score;
-        private static CheckerPiece[] m_CheckersPiece;
+        private static CheckersPiece[] m_CheckersPiece;
         private readonly char m_CheckerPieceKind;
+        private readonly short m_PlayerNumber;
+
+        // Enums:
+        public enum ePlayerType
+        {
+            MainPlayer = 0,
+            RivalPlayer = 1
+        }
 
 
         // Constructors:
-        public User(string i_Name, char i_CheckerKind)
+        public User(string i_Name, short i_PlayerNumber, char i_CheckerKind)
         {
             m_Name = i_Name;
             m_Score = 0;
             m_CheckerPieceKind = i_CheckerKind;
+            m_PlayerNumber = i_PlayerNumber;
         }
 
         // Properties:
@@ -48,7 +57,7 @@ namespace Player
             }
         }
 
-        public CheckerPiece[] Pieces
+        public CheckersPiece[] Pieces
         {
             get
             {
@@ -70,24 +79,30 @@ namespace Player
         {
             int sizeOfPieces = ((i_BoardSize / 2) * 3);
 
-            m_CheckersPiece = new CheckerPiece[sizeOfPieces];
+            m_CheckersPiece = new CheckersPiece[sizeOfPieces];
         }
 
-        public void Play(string i_CheckerPosition, string i_CheckerNextPosition)
+        public void Play(ref Board i_GameBoard, User i_RivalPlayer , string i_CheckerPosition, string i_CheckerNextPosition)
         {
             // First, check if the given input is valid.
-            Validation.CheckValidInput(Name, ref i_CheckerPosition, ref i_CheckerNextPosition);
+            Validation.CheckValidInput(Name,
+                ref i_CheckerPosition,
+                ref i_CheckerNextPosition, 
+                (int)i_GameBoard.SizeOfBoard);
 
             // Second, finds the match checker piece, with it's position.
-            CheckerPiece? currentCheckerPiece = findCheckerPiece(i_CheckerPosition);
+            CheckersPiece? currentCheckerPiece = findCheckerPiece(i_CheckerPosition);
 
             // Checks if the checker piece was found.
             if (currentCheckerPiece != null)
             {
                 // Make the moves that the user wanted. Regular move or King move.
-                if (isRegularChecker(currentCheckerPiece.Value))
+                if (!currentCheckerPiece.Value.IsKing)
                 {
-                    MoveUtils.MoveRegularTool(i_CheckerPosition, i_CheckerNextPosition);
+                    MoveUtils.MoveRegularTool(this, i_RivalPlayer,
+                                              ref i_GameBoard,
+                                              currentCheckerPiece.Value,
+                                              i_CheckerNextPosition);
                 }
                 else
                 {
@@ -96,9 +111,9 @@ namespace Player
             }
         }
 
-        private static CheckerPiece? findCheckerPiece(string i_CurrentPosition)
+        private static CheckersPiece? findCheckerPiece(string i_CurrentPosition)
         {
-            CheckerPiece? currentCheckerPiece = null;
+            CheckersPiece? currentCheckerPiece = null;
 
             foreach (var piece in m_CheckersPiece)
             {
@@ -107,12 +122,6 @@ namespace Player
             }
 
             return currentCheckerPiece;
-        }
-
-        private static bool isRegularChecker(CheckerPiece i_CheckerPiece)
-        {
-            return i_CheckerPiece.PieceKind == CheckerPiece.ePieceKind.X ||
-                   i_CheckerPiece.PieceKind == CheckerPiece.ePieceKind.O;
         }
 
         public static void Quit(char i_QuitInput)
