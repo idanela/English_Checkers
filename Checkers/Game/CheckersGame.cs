f73e1fa9536df4b2643c6f606d029b03b88ae0ff
idanela <58819088+idanela@users.noreply.github.com>
2020-08-30 21:33:43 +0300

    Adding UI

--- Checkers/Game/CheckersGame.cs
+++ Checkers/Game/CheckersGame.cs
@@ -1,21 +1,18 @@
 ﻿using System;
 using Player;
-using CheckersBoard; 
+using CheckersBoard;
+using UI;
 namespace Game
 {
     public class CheckersGame
     {
         public static void  runGame()
-        {  
-            Console.WriteLine("Please enter your name:");
-            string name = getValidPlayerName();
-
+        {
+            string name = UserIntterface.GetValidUserName();
             User firstPlayer = new User(name, 'X'); 
-            Console.WriteLine("Please enter the size of the Board");
-            ushort BoardSize = getValidBoardSize();
-
+            ushort BoardSize = UserIntterface.GetValidBoardSize();
             Board gameBoard = new Board(BoardSize);
-            char choice = getContestent();
+            char choice = UserIntterface.GetRival();
 
             gameBoard.printBoard();
             if(choice == 1)
@@ -28,73 +25,9 @@ namespace Game
             }
         }
 
-        private static char getContestent()
-        {
-            Console.WriteLine(@"Please pick the game kind
-1. Two players.
-2. VS computer.");
-
-            char choice = getValidchoice(Console.ReadLine());
-
-            return choice;
-        }
-
-        private static string getValidPlayerName()
-        {
-            string playerName = Console.ReadLine();
-
-            while (IsValidUserName(playerName))
-            {
-                Console.WriteLine("Invalid Input please type your name again.");
-                playerName = Console.ReadLine();
-            }
-
-            return playerName;
-        }
-
-        private static ushort getValidBoardSize()
-        {
-            ushort sizeOfBoard;
-            while (!ushort.TryParse(Console.ReadLine(), out sizeOfBoard) || !CheckersBoard.Board.isValidBoardSize(sizeOfBoard))
-            {
-                Console.WriteLine("Invalid input please insert size again.");
-                ushort.TryParse(Console.ReadLine(), out sizeOfBoard);
-            }
-
-            return sizeOfBoard;
-        }
-
-        private static char getValidchoice(string PlayerChoice)
-        {
-            char choice;
-            while (!char.TryParse(PlayerChoice, out choice) && choice != '1' && choice != '2')
-            {
-                Console.WriteLine("invalid choice. please try again:");
-                PlayerChoice = Console.ReadLine();
-            }
-
-            return choice;
-        }
-
-        private static bool IsValidUserName(string nameOfPlayer)
-        {
-            bool isValidName = nameOfPlayer.Length >20 && nameOfPlayer.Length > 0;
-
-            if(isValidName)
-            {
-                for (int i = 0; i < nameOfPlayer.Length; i++)
-                {
-                    isValidName = isValidName && char.IsLetter(nameOfPlayer[i]);
-                }
-            }
-
-            return isValidName;
-        }
-
         private static void playVsAnotherPlayer(User i_FirstPlayer)
         {
-            Console.WriteLine("Please enter second player's name:");
-            string name = getValidPlayerName();
+            string name = UserIntterface.GetValidUserName();
             User secondPlayer = new User(name, 'O'); 
             bool hasGameFinished = false;
             bool isFirstPlayerTurn = true;
---
+++ Checkers/UI/UserIntterface.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace UI
+{
+   public class UserIntterface
+    {
+        public static string GetValidUserName()
+        {
+            Console.WriteLine("Please enter player's name:");
+            return getValidPlayerName();
+        }
+
+        private static string getValidPlayerName()
+        {
+            string playerName = Console.ReadLine();
+
+            while (IsValidUserName(playerName))
+            {
+                Console.WriteLine("Invalid Input please type your name again.");
+                playerName = Console.ReadLine();
+            }
+
+            return playerName;
+        }
+            private static bool IsValidUserName(string nameOfPlayer)
+            {
+                bool isValidName = nameOfPlayer.Length > 20 && nameOfPlayer.Length > 0;
+
+                if (isValidName)
+                {
+                    for (int i = 0; i < nameOfPlayer.Length; i++)
+                    {
+                        isValidName = isValidName && char.IsLetter(nameOfPlayer[i]);
+                    }
+                }
+
+                return isValidName;
+            }
+
+        public static ushort GetValidBoardSize()
+        {
+            Console.WriteLine("Please enter the size of the Board");
+            return getValidBoardSize();
+        }
+
+        private static ushort getValidBoardSize()
+        {
+            ushort sizeOfBoard;
+            while (!ushort.TryParse(Console.ReadLine(), out sizeOfBoard) || !CheckersBoard.Board.isValidBoardSize(sizeOfBoard))
+            {
+                Console.WriteLine("Invalid input please insert size again.");
+                ushort.TryParse(Console.ReadLine(), out sizeOfBoard);
+            }
+
+            return sizeOfBoard;
+        }
+
+        public static char GetRival()
+        {
+            Console.WriteLine(@"Please pick the game kind
+1. Two players.
+2. VS computer.");
+
+            return getCompetitor();
+        }
+
+        private static char getCompetitor()
+        {
+            char choice = getValidchoice(Console.ReadLine());
+
+            return choice;
+        }
+
+        private static char getValidchoice(string PlayerChoice)
+        {
+            char choice;
+            while (!char.TryParse(PlayerChoice, out choice) && choice != '1' && choice != '2')
+            {
+                Console.WriteLine("invalid choice. please try again:");
+                PlayerChoice = Console.ReadLine();
+            }
+
+            return choice;
+        }
+    }
+}
